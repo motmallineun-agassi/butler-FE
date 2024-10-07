ae9978a4fba17f6d08c1d259b238ec66cb18eb7e
piaoyanxiu <135508811+piaoyanxiu@users.noreply.github.com>
2024-10-07 23:30:08 +0900

    Delete src directory

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./main";
-export * from "./butler";
-export * from "./miyeonshi";
--- src/components/main/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./main";
-export * from "./name";
-export * from "./header";
-export * from "./tutorial";
